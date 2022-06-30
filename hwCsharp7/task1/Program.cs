a2fa5e73856f764fdb9ea9f68cbaec7cb0da10c4
MaximKharin <maximkharin@gmail.com>
2022-06-30 18:09:55 +0300

    finished task 1

---
+++ hwCsharp7/task1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 1: Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Write("Enter your number, please: ");
+
+int[] array = InitArray();
+int count = CountPositiveNumbers();
+Console.WriteLine($"Number of numbers greater then zero is {count}");
+
+int[] InitArray()
+{
+    char[] delimiterChars = { ' ', ',', ':', '\t'};
+    string[] stringArray = Console.ReadLine().Split(delimiterChars);
+    return Array.ConvertAll(stringArray, int.Parse);
+}
+
+int CountPositiveNumbers()
+{
+    int count = 0;
+    foreach (int element in array)
+    {
+        if (element > 0) count++;
+    }
+
+    return count;
+}
